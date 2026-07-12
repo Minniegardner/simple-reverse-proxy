0a82e29a6823bdd99c577770f36605e048f93cfa
Minie Gardner <carriemcbride4408@gmail.com>
2026-07-12 04:59:17 -0700

    Update IndexRoute.js

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -1,9 +1,26 @@
 // importing necessary modules
 const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
 
-// setting the route
+// Lokasi miss.txt
+const MISS_FILE = path.join(__dirname, "miss.txt");
+
+// Home
 router.get("/", (req, res) => {
     res.send("Simple reverse proxy by GrowPlus Community");
+});
+
+// Akses miss.txt
+router.get("/miss.txt", (req, res) => {
+    fs.readFile(MISS_FILE, "utf8", (err, data) => {
+        if (err) {
+            return res.status(404).send("miss.txt not found");
+        }
+
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.send(data);
+    });
 });
 
 // exporting the router
